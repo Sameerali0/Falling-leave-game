47a14978884da21d19d16034b497885f395d6a29
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-23 00:27:54 +0500

    set player image on canvas

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 560;
+
+const player = {
+    x: canvas.width / 2 - 75,
+    y: canvas.height - 140,
+    width: 150,
+    height: 150,
+    image: new Image(),
+}
+
+player.image.src = "images/player.png";
+
+function drawPlayer() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(player.image, player.x, player.y, player.width, player.height)
+}
+
+function game() {
+    drawPlayer();
+    requestAnimationFrame(game);
+}
+
+game();
