6e99ada98b674667befc29ab2d63a0b8c778daca
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-23 20:26:10 +0500

    add falling leaves animation

--- script.js
+++ script.js
@@ -16,11 +16,13 @@ const player = {
 player.image.src = "images/player.png";
 
 function drawPlayer() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(player.image, player.x, player.y, player.width, player.height)
 }
 
 function game() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    updateLeaves();
+    drawLeaves();
     playerMovement();
     drawPlayer();
     requestAnimationFrame(game);
@@ -48,6 +50,42 @@ function playerMovement() {
 
     if (keys.right && player.x + player.width < canvas.width) {
         player.x += player.speed;
+    }
+}
+
+const leaves = [];
+
+const leafImage = new Image();
+leafImage.src = "images/greenLeaf.png";
+
+function createLeaf() {
+  const size = 40 + Math.random() * 20;
+  const x = Math.random() * (canvas.width -size)
+  const y = - size;
+  const speed = 2 + Math.random() * 2;
+  leaves.push({ x, y, size, speed})
+}
+
+function drawLeaves() {
+  for (let i = 0; i < leaves.length; i++) {
+    const leaf = leaves[i];
+    ctx.drawImage(leafImage, leaf.x, leaf.y, leaf.size, leaf.size)
+  }
+}
+
+function updateLeaves() {
+    for (let i = 0; i < leaves.length; i++) {
+        leaves[i].y += leaves[i].speed;
+    }
+
+    for (let i = leaves.length - 1; i >= 0; i --) {
+        if (leaves[i].y > canvas.height) {
+            leaves.splice(i, 1);
+        }
+    }
+
+    if (Math.random() < 0.03) {
+        createLeaf();
     }
 }
 
